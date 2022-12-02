e633db47b401b941c80e07711b0c2d0510be9bc6
Mahrjose <mahrjose.dev@gmail.com>
2022-12-02 22:44:14 +0600

    Make `Qtile` better - Add ArchLinux icon - Add specific emoji for specific workspaces instead of numbers - Fix current window name getting out of hand:v ( Now only show current window name) - Fix and Change widget theme colors - Change clock widget looks - Add icon for shutdown/Logout

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -27,9 +27,9 @@ from libqtile.lazy import lazy
 import colors
 
 
-# mod4 is your window key / super key
+# mod4 is the window key / super key
 mod = "mod4"
-# mod1 is your left 'alt' key
+# mod1 is the left 'alt' key
 alt = "mod1"
 
 # My Softwares
@@ -37,13 +37,6 @@ terminal = "alacritty"
 browser = "google-chrome-stable"
 videoPlayer = "vlc"
 primaryMenu = "rofi -show drun"
-
-
-# This script will start on log in
-@hook.subscribe.startup_once
-def autostart():
-    home = os.path.expanduser("~/.config/qtile/autostart.sh")
-    subprocess.run([home])
 
 
 keys = [
@@ -107,7 +100,7 @@ keys = [
     # Browser
     Key([mod, "shift"], "b", lazy.spawn(browser), desc="Launch Default Browser"),
     # rofi
-    Key([mod], "space", lazy.spawn("rofi -show drun"), desc="Launch rofi"),
+    Key([mod], "space", lazy.spawn(primaryMenu), desc="Launch rofi"),
     #
     # =============================================================================== #
     #                       >> Dedicatd Volume key controls <<                        #
@@ -150,22 +143,31 @@ keys = [
     Key([], "XF86MonBrightnessDown", lazy.spawn("lux -s 5%")),
 ]
 
-groups = [Group(i) for i in "123456789"]
+group_layout = "bsp"
+groups = [
+    Group(name = "1",label = " ", layout=group_layout),
+    Group(name = "2",label = " ", layout=group_layout),
+    Group(name = "3",label = " ", layout=group_layout),
+    Group(name = "4",label = " ", layout=group_layout),
+    Group(name = "5",label = " ", layout=group_layout),
+    Group(name = "6",label = " ", layout=group_layout),
+    Group(name = "7",label = " ", layout=group_layout),
+    Group(name = "8",label = " ", layout=group_layout),
+    Group(name = "9",label = " ", layout=group_layout),
+]
 
 for i in groups:
     keys.extend(
         [
             # mod1 + letter of group = switch to group
             Key(
-                [mod],
-                i.name,
+                [mod], i.name,
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
-                [mod, "shift"],
-                i.name,
+                [mod, "shift"], i.name,
                 lazy.window.togroup(i.name, switch_group=True),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
@@ -178,25 +180,35 @@ for i in groups:
 
 
 # Setting up default border themes
-def init_layout_theme():
-    return {
-        'margin': 5,
+def init_layout_theme(max=False):
+    if max:
+        return {
+        'margin': 10,
         'border_width': 3,
-        'border_focus': '#5e81ac',
-        'border_normal': '#4c566a'
+        'border_focus': '#0DE77C',
     }
+    else:
+        return {
+            'margin': 8,
+            'border_width': 3,
+            'border_focus': '#5e81ac',
+            'border_normal': '#4c566a'
+        }
+        
+
 
 layout_theme = init_layout_theme()
+max_layout_theme = init_layout_theme(max=True)
 
 layouts = [
-    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Max(**max_layout_theme),
     layout.Columns(**layout_theme),
-    layout.Max(**layout_theme),
+
     # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
+    # layout.Bsp(**layout_theme),
     # layout.MonadTall(**layout_theme),
+    # layout.Stack(num_stacks=2),
+    # layout.Matrix(),
     # layout.floating.Floating(**layout_theme),
     # layout.MonadWide(),
     # layout.RatioTile(),
@@ -206,66 +218,155 @@ layouts = [
     # layout.Zoomy(),
 ]
 
+colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo = colors.dracula()
 
 widget_defaults = dict(
-    font="sans",
-    fontsize=12,
-    padding=3,
+    font = "Hack",
+    fontsize = 12,
+    padding = 2,
+    background = backgroundColor
 )
 extension_defaults = widget_defaults.copy()
 
-colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo = colors.dracula()
+# Screens & Widget related fuctions
+def shortenWindowName(text):
+    nameArr = text.split(" - ")
+    return nameArr[-1]
 
-
+# Creating the screen
 screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
+                widget.Image(
+                    filename = './icons/ArchLinux_Logo.png',
+                    scale = 'False',
+                    margin_x = 4,
+                    # mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(file_launcher2)}
+                ),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 4,
+                    foreground = '#f8f8f2'
+                ),
+                widget.GroupBox(
+                    padding = 2,
+					active = colors[2],
+                    inactive = colors[1],
+                    highlight_color = [backgroundColor, workspaceColor],
+                    highlight_method = 'line',
+                ),                
+                # widget.Sep(
+                #     linewidth = 2,
+                #     padding = 1,
+                #     foreground = '#f8f8f2'
+                # ),
+                # widget.CurrentLayout(
+				# 	scale = 0.6,
+                # ),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 4,
+                    foreground = '#f8f8f2'
+                ),
                 widget.Prompt(),
-                widget.WindowName(),
+                widget.WindowName(
+                    font = 'Ubuntu Bold',
+                    parse_text = shortenWindowName
+                ),
                 widget.Chord(
                     chords_colors={
                         "launch": ("#ff0000", "#ffffff"),
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+                # Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
                 widget.Systray(),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 4,
+                    foreground = '#f8f8f2'
+                ),                
                 widget.Net(
+                    font = "Ubuntu Bold",
                     interface='wlo1',
+                    format = ' NET: {down} ↓↑ {up}',
                     mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn('connman-gtk')},
-                    background = foregroundColorTwo,
-                    foreground = colors[6]
+                    foreground = colors[6],
+                ),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 3,
+                    foreground = '#f8f8f2'
                 ),
                 widget.CPU(
+                    font = "Ubuntu Bold",
                     format = 'CPU: {load_percent}%',
-                    background = foregroundColorTwo,
-                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + ' -e htop')},
                     foreground = colors[10],
-                    padding = 13
+                    padding = 13,
+                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + ' -e htop')},
+                ),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 4,
+                    foreground = '#f8f8f2'
                 ),
                 widget.Memory(
-                    background = foregroundColorTwo,
+                    font = "Ubuntu Bold",
                     foreground = colors[4],
-                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + ' -e htop')},
                     fmt = 'RAM: {}',
-                    padding = 13
+                    padding = 8,
+                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(terminal + ' -e htop')},
                 ),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 2,
+                    foreground = '#f8f8f2'
+                ),
+                widget.Volume(
+                    fmt = ' {}',
+                    padding = 8,
+                    foreground = colors[8],
+                    ),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 4,
+                    foreground = '#f8f8f2'
+                ),
+
                 widget.Battery(
                     charge_char ='',
                     discharge_char = '',
                     format = '  {percent:2.0%} {char}',
                     foreground = colors[5],
-                    padding = 13,
-                    background = foregroundColorTwo
+                    padding = 8,
                 ),
-                widget.Clock(format="%d-%m %a %I:%M %p", background = foregroundColorTwo),
-                widget.QuickExit(background = foregroundColorTwo),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 4,
+                    foreground = '#f8f8f2'
+                ),
+                #widget.Clock(format="%d-%m %a %I:%M %p", background = foregroundColorTwo),
+                 widget.Clock(format=' %a, %d/ %m/ %Y. |  %I:%M %p',
+					foreground = colors[2],
+                    background = backgroundColor,
+					padding = 8
+				),
+                widget.Sep(
+                    linewidth = 2,
+                    padding = 4,
+                    foreground = '#f8f8f2'
+                ),
+                # widget.QuickExit(background = foregroundColorTwo),
+				widget.QuickExit(
+					fmt = ' ',
+					foreground = colors[9],
+					padding = 8
+					),   
+                
             ],
-            22,
+            23,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
@@ -296,6 +397,14 @@ floating_layout = layout.Floating(
         Match(title="pinentry"),  # GPG key password entry
     ]
 )
+
+# This script will start on log in
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser("~/.config/qtile/autostart.sh")
+    subprocess.run([home])
+
+
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
