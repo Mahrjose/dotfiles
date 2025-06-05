0910dcc146ac5aca711562dc9a55deb2307d9d0f
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2025-06-05 16:06:19 +0600

    Add Legecy Files

---
+++ archive/wm/qtile/dependencies.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/sh
+#----------------------------------------------------
+# Author: Mirza Mahrab Hossain
+# Email	: mahrjose.dev@gmail.com
+# Github: https://github.com/Mahrjose/dotfiles
+#----------------------------------------------------
+#
+# Requirement for the built-in 'Net' & 'CPU' widget
+pip install psutil
